e1bbca6d0347b700bcc0fa2df555994d58065b99
Emir Kurtuldu <emirkurtuldu@icloud.com>
2024-03-30 11:44:13 +0300

    make some changes on main and add csv file

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 class Item:
-    def __init__(self, name, price, quantity):
+    all = []
+    
+    def __init__(self, name: str, price: float, quantity=0):
+        #run validations to the recevied argument
+        assert price >= 0, f"price {price} is not greater than 0"
+        assert quantity >= 0, f"quantity {quantity} is not greater than 0"
+    
+        #assignt to self object
         self.name = name
         self.price = price
         self.quantity = quantity
-    def calculate_total_price(self, x, y):
-        return x * y
+    
+        #Action to execute
+        Item.all.append(self)
+    
+    def calculate_total_price(self):
+        return self.price * self.quantity
+        
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
+    def __repr__(self):
+        return ("Item'{}', {}, {}").format((self.name), (self.price), (self.quantity))
 
-item1 = Item("Phone", 100, 5)
-
-
+item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-print(item1.name)
-print(item2.name)
-print(item1.price)
-print(item2.price)
-print(item1.quantity)
-print(item2.quantity)
+print(Item.all)
