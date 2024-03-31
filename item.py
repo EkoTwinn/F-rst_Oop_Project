78e512c879f240b4372ce1d04daf38122d4c0409
Emir Kurtuldu <emirkurtuldu@icloud.com>
2024-03-31 15:44:40 +0300

    the version for uploading to github

--- item.py
+++ item.py
@@ -2,19 +2,28 @@ import csv
 
 class Item:
     all = []
-    
+
     def __init__(self, name: str, price: float, quantity=0):
-        #run validations to the recevied argument
+        # Run validations to the received arguments
         assert price >= 0, f"price {price} is not greater than 0"
         assert quantity >= 0, f"quantity {quantity} is not greater than 0"
-    
-        #assignt to self object
-        self.name = name
+
+        # Assign to self object
+        self._name = name
         self.price = price
         self.quantity = quantity
-    
-        #Action to execute
+
+        # Action to execute
         Item.all.append(self)
+
+    @property
+    def name(self):
+        """Returns the name of the item."""
+        return self._name
+        
+
+
+
     
     def calculate_total_price(self):
         return self.price * self.quantity
@@ -50,3 +59,5 @@ class Item:
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
+    
